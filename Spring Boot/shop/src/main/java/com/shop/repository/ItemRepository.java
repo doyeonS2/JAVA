e7319c50c90b87326baf474edc51b1b54f1cd245
doyeon <1162004@naver.com>
2022-11-17 15:49:12 +0900

    COMMIT

--- Spring Boot/shop/src/main/java/com/shop/repository/ItemRepository.java
+++ Spring Boot/shop/src/main/java/com/shop/repository/ItemRepository.java
@@ -3,6 +3,13 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmLike(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+    List<Item> findByPriceLessThan(Integer price); // 파라미터로 넘어온 값보다 작은 상품 조회
 
 }
